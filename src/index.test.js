9afda0b249ce9b06e2c11b4a634c711a31215205
Nick Wooster <nicholaswooster@gmail.com>
2018-04-04 15:17:02 -0600

    module 9 work in progress

--- src/index.test.js
+++ src/index.test.js
@@ -1,5 +1,5 @@
 import {expect} from 'chai';
-import jsdom from 'jsdom';
+import jsdom from 'jsdom/lib/old-api';
 import fs from 'fs';
 
 describe('Our first test', () => {
@@ -9,11 +9,12 @@ describe('Our first test', () => {
 });
 
 describe('index.html', () => {
-    it('should say hello', () => {
+    it('should say hello', (done) => {
        const index = fs.readFileSync('./src/index.html', "utf-8");
-       jsdom.env(index, function(err, window) {
+       jsdom.env(index, function(errors, window) {
            const h1 = window.document.getElementsByTagName('h1')[0];
            expect(h1.innerHTML).to.equal("Hello World!");
+           done();
            window.close();
        });
     });
